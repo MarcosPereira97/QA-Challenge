725b9a3ebaeb7fe1ab7491226889b352827c1c01
MarcosPereira97 <marcos_henriquejunior@hotmail.com>
2023-02-26 00:39:51 -0300

    Preenchendo formulario checkout com faker

--- cypress/e2e/cenario1.cy.js
+++ cypress/e2e/cenario1.cy.js
@@ -1,12 +1,18 @@
 /// <reference types="cypress" />
+import { faker } from '@faker-js/faker';
 
 describe('cenario 1', () => {
-  let testData;
 
-  before(() => {
-    cy.fixture('env').then(t => {
-      testData = t
-    })
+  const firstName = faker.name.firstName()
+  const lastName = faker.name.lastName()
+
+
+  Cypress.$.ajax({
+    url: 'https://randomuser.me/api/',
+    dataType: 'json',
+    success: function (data) {
+      console.log(data);
+    }
   });
 
   context('compra de produto', () => {
@@ -18,7 +24,7 @@ describe('cenario 1', () => {
     it('deve estar na página Luma Store', () => {
       cy.title()
         .should('contain', 'Home Page - Magento eCommerce - website to practice selenium')
-      cy.get('#search').type(testData.produto + '{enter}')
+      cy.get('#search').type('shirt', '{enter}')
       cy.get('div[class*=products-grid]')
         .should('be.visible')
 
@@ -27,20 +33,35 @@ describe('cenario 1', () => {
         .click()
 
       cy.get('.product-add-form')
-        .wait(3000)
+        .wait(2500)
         .within(() => {
 
           cy.get('div')
             .contains('M')
             .click()
-            .wait(1000)
 
           cy.get('#option-label-color-93-item-50').click()
-
           cy.contains('button', 'Add to Cart').click()
         })
 
-        cy.get('.message-success').should('be.visible')
-    });
+      cy.get('.message-success')
+        .should('be.visible')
+
+      cy.contains('a', 'shopping cart').click()
+
+      cy.get('ul[class*=checkout-methods-items]')
+        .wait(1000)
+        .click()
+
+      cy.get('#checkout-step-shipping').within(() => {
+        cy.get('input[name=username]').type(faker.internet.email(firstName, lastName))
+        cy.get('input[name=firstname]').type(firstName)
+        cy.get('input[name=lastname]').type(lastName)
+        cy.get('input[name*=street]')
+          .first()
+          .type(faker.address.street())
+        cy.get('input[name=city]').type(faker.address.city())
+      })
+    })
   })
 })
